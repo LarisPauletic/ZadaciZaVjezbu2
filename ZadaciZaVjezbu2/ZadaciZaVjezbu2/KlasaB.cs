61f1ba463537660d0a0692d4276c0431c8b20ee7
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-12-11 16:40:46 +0100

    Add files via upload

---
+++ ZadaciZaVjezbu2/ZadaciZaVjezbu2/KlasaB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu2
+{
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
---
+++ ZadaciZaVjezbu2/ZadaciZaVjezbu2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string s = Console.ReadLine();
+            KlasaB x = new KlasaB();
+            if (s.Length > 2)
+                s = x.BezPrvogIZadnjeg(s);
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+
+        }
+    }
+}
